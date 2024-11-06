6ec1dbef1a44c482e5dcd902e9f71e7704d82059
uyiyiyi <670434852@qq.com>
2024-11-06 14:12:09 +0800

    first commit

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yzc/fcl
-# Build directory: /home/yzc/fcl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include/fcl")
-subdirs("src")
-subdirs("test")
