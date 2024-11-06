98b96540923521f5bae426dce990b0c252d1781b
uyiyiyi <670434852@qq.com>
2024-11-06 14:11:18 +0800

    first commit

---
+++ build/test/CMakeFiles/test_fcl_capsule_capsule.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_fcl_capsule_capsule.dir/test_fcl_capsule_capsule.cpp.o"
+  "test_fcl_capsule_capsule"
+  "test_fcl_capsule_capsule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_fcl_capsule_capsule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,34 @@
+# CMake generated Testfile for 
+# Source directory: /home/yzc/fcl/test
+# Build directory: /home/yzc/fcl/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_fcl_collision "/test_fcl_collision")
+set_tests_properties(test_fcl_collision PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;34;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_distance "/test_fcl_distance")
+set_tests_properties(test_fcl_distance PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;35;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_geometric_shapes "/test_fcl_geometric_shapes")
+set_tests_properties(test_fcl_geometric_shapes PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;36;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_broadphase "/test_fcl_broadphase")
+set_tests_properties(test_fcl_broadphase PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;37;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_shape_mesh_consistency "/test_fcl_shape_mesh_consistency")
+set_tests_properties(test_fcl_shape_mesh_consistency PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;38;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_frontlist "/test_fcl_frontlist")
+set_tests_properties(test_fcl_frontlist PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;39;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_math "/test_fcl_math")
+set_tests_properties(test_fcl_math PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;40;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_sphere_capsule "/test_fcl_sphere_capsule")
+set_tests_properties(test_fcl_sphere_capsule PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;42;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_capsule_capsule "/test_fcl_capsule_capsule")
+set_tests_properties(test_fcl_capsule_capsule PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;43;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_simple "/test_fcl_simple")
+set_tests_properties(test_fcl_simple PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;44;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_capsule_box_1 "/test_fcl_capsule_box_1")
+set_tests_properties(test_fcl_capsule_box_1 PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;45;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_capsule_box_2 "/test_fcl_capsule_box_2")
+set_tests_properties(test_fcl_capsule_box_2 PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;46;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_bvh_models "/test_fcl_bvh_models")
+set_tests_properties(test_fcl_bvh_models PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;48;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
+add_test(test_fcl_octomap "/test_fcl_octomap")
+set_tests_properties(test_fcl_octomap PROPERTIES  _BACKTRACE_TRIPLES "/home/yzc/fcl/test/CMakeLists.txt;18;add_test;/home/yzc/fcl/test/CMakeLists.txt;51;add_fcl_test;/home/yzc/fcl/test/CMakeLists.txt;0;")
